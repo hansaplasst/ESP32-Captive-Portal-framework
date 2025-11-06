d0cb95a8b0ec45e66e6cbaed80e8c742b0226629
hansaplasst <forum@logic-q.nl>
2025-11-06 01:26:08 +0100

    modified:   .gitignore 	new file:   examples/main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <CaptivePortal.h>
+
+#include "Config.h"
+CaptivePortal* portal = nullptr;
+
+void setup() {
+  portal = new CaptivePortal();
+  portal->begin("ESP32-CaptivePortal");
+}
+
+void loop() {
+  if (portal) {
+    portal->handle();
+  }
+}
